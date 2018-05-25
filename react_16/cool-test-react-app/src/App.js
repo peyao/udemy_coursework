744f60dd6d644bc7df7839e82a8512c7a9e0be5e
peyao <yaop@vmware.com>
2018-05-24 19:01:17 -0700

    Prep for react_16 section 7 refactor

--- react_16/cool-test-react-app/src/App.js
+++ react_16/cool-test-react-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -48,52 +49,47 @@ class App extends Component {
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
+
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
+              <ErrorBoundary
                 key={person.id}
-                name={person.name}
-                age={person.age}
-                click={() => this.deletePersonHandler(index)}
-                change={(event) => this.nameChangedHandler(event, person.id)}
-              />
+              >
+                <Person
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.deletePersonHandler(index)}
+                  change={(event) => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi I'm a react app</h1>
-        <p className={classes.join(' ')}>This is working.</p>
+        <p className={assignedClasses.join(' ')}>This is working.</p>
         <button
+          className={btnClass}
           onClick={this.togglePersonsHandler}
-          style={style}
         >
           Toggle Persons
         </button>
--- react_16/cool-test-react-app/src/Person/Person.js
+++ react_16/cool-test-react-app/src/Person/Person.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // import { Input } from 'reactstrap';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
+  const rand = Math.random();
+  if (rand > 0.7) {
+    throw new Error('Something went wrong!');
+  }
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.change} value={props.name} />
